81a951aa5b8a7595b894ab0eecf7726e8d81e2b8
SubashSyngrid <subashm@syngrid.com>
2025-09-25 14:26:19 +0530

    verceljson

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
-app.use('/auth', authRoutes);
+
+app.use('/', authRoutes);
 app.use('/attendance', attendanceRoutes);
 app.use('/tasks', taskRoutes);
 // app.use('/documents', documentRoutes);
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,6 +3,12 @@ import { register, login } from "../controllers/authController";
 
 const router = Router();
 
+router.get('/', function(req, res, next) {
+    console.log("hello world");
+    
+  res.send('respond with a resource');
+});
+
 router.post("/register", register);
 router.post("/login", login);
 
